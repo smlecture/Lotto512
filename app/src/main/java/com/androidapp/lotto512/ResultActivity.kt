0012a43e780a1fa82e977c09023103d31946cf15
H-J Yoo <yoohj@smu.ac.kr>
2021-05-12 10:37:18 +0900

    Practice switching activities

--- app/src/main/java/com/androidapp/lotto512/ResultActivity.kt
+++ app/src/main/java/com/androidapp/lotto512/ResultActivity.kt
@@ -2,10 +2,12 @@ package com.androidapp.lotto512
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        Toast.makeText(this, "Result Activity", Toast.LENGTH_LONG).show()
     }
 }
