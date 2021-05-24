1e1e4bedd856c5f457fb610acb6d4505fbe01af5
H-J Yoo <yoohj@smu.ac.kr>
2021-05-24 18:38:58 +0900

    ball image update

--- app/src/main/java/com/androidapp/lotto512/ResultActivity.kt
+++ app/src/main/java/com/androidapp/lotto512/ResultActivity.kt
@@ -16,18 +16,13 @@ class ResultActivity : AppCompatActivity() {
         val result = intent.getIntegerArrayListExtra("result") // ?: return
 
         // 2) sort the array
-//        val result_sorted = result?.let { result.sortedBy{it} }
-        result?.let{
-            updateLottoBallImages(result.sortedBy{it})
-        }
+        val result_sorted = result?.let { result.sortedBy{it} }
+/*        result?.let{
+//            updateLottoBallImages(result.sortedBy{it})
+            updateLottoBallImages(result_sorted)
+        }*/
 
-        // 3) set images
-
-
-    }
-
-//    private fun updateLottoBallImages(result_sorted : ArrayList<Int>) {
-    private fun updateLottoBallImages(result_sorted : List<Int>) {
+        // 3) update images
         val lottoImageStartId = R.drawable.ball_01
 
         val imageView1 = findViewById<ImageView>(R.id.imageView1)
@@ -43,5 +38,25 @@ class ResultActivity : AppCompatActivity() {
         imageView4.setImageResource(lottoImageStartId + result_sorted?.get(3) - 1)
         imageView5.setImageResource(lottoImageStartId + result_sorted?.get(4) - 1)
         imageView6.setImageResource(lottoImageStartId + result_sorted?.get(5) - 1)
+
     }
+
+//    private fun updateLottoBallImages(result_sorted : ArrayList<Int>) {
+/*    private fun updateLottoBallImages(result_sorted : List<Int>) {
+        val lottoImageStartId = R.drawable.ball_01
+
+        val imageView1 = findViewById<ImageView>(R.id.imageView1)
+        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+        val imageView3 = findViewById<ImageView>(R.id.imageView3)
+        val imageView4 = findViewById<ImageView>(R.id.imageView4)
+        val imageView5 = findViewById<ImageView>(R.id.imageView5)
+        val imageView6 = findViewById<ImageView>(R.id.imageView6)
+
+        imageView1.setImageResource(lottoImageStartId + result_sorted?.get(0)!! - 1)
+        imageView2.setImageResource(lottoImageStartId + result_sorted?.get(1) - 1)
+        imageView3.setImageResource(lottoImageStartId + result_sorted?.get(2) - 1)
+        imageView4.setImageResource(lottoImageStartId + result_sorted?.get(3) - 1)
+        imageView5.setImageResource(lottoImageStartId + result_sorted?.get(4) - 1)
+        imageView6.setImageResource(lottoImageStartId + result_sorted?.get(5) - 1)
+    }*/
 }
